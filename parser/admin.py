b13080d937306e0cbfa59f4657a1b38358fb7d61
黃志仁 <tbsthitw@gmail.com>
2018-06-06 04:26:22 +0800

    COOOOOOOOOOL

---
+++ parser/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Courses
+
+# Register your models here.
+admin.site.register(Courses)
---
+++ parser/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ParserConfig(AppConfig):
+    name = 'parser'
