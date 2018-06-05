b950a9fe605b5a121c5750c2af598165d50dc063
HankLu <s210215@shsh.tw>
2018-06-06 06:48:12 +0800

    Fix: folder

--- taiwan_tech_course/taiwan_tech_course/parser/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Courses
-
-# Register your models here.
-admin.site.register(Courses)
--- taiwan_tech_course/taiwan_tech_course/taiwan_tech_course/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url, include, url
-from django.contrib import admin
-
-
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'timetable/', include('timetable.urls')),
-    url(r'^accounts/', include('allauth.urls')),
-]
